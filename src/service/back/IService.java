f8f214e2a17b30b3a2a549ba7212ec81e7ba51c5
lhj <237377116@qq.com>
2017-10-09 18:33:56 +0800

    swqw

--- src/service/back/IService.java
+++ src/service/back/IService.java
@@ -5,9 +5,7 @@ import java.util.Set;
 
 public interface IService<K, V> {
     boolean insert(V vo) throws Exception;
-
     List<V> list() throws Exception;
-
     boolean update(V vo) throws Exception;
     boolean delete(Set<K> ids)throws Exception;
 }
